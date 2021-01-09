c2570c4f05f299d8e7363e1348f946b3f52061cd
Nikhil Ivannan <nickonicko779@gmail.com>
2021-01-09 17:23:37 +0530

    re-worked on actual.py

--- actual.py
+++ actual.py
@@ -1,110 +1,113 @@
-import pickle, os, random
+import pickle
+import os
+import random
+from cryptography.fernet import Fernet
 
-class Student:
 
-    def __init__(self, roll, name, password):
-        self.roll = roll
-        self.name = name
+class drop:
+    def __init__(self, no, purp, password, key):
+        self.no = no
+        self.purp = purp
         self.password = password
+        self.key = key
 
-    def __str__(self):
-        return 'Name:%5s\nPassword:%4s'%(self.name, self.password)
+    def __repr__(self):
+        return self.password.decode()
 
-class Students:
+
+class main:
 
     def __init__(self):
-        self.filename = 'passwords.dat'
+        self.filename = "passwords.bin"
         self.load()
 
     def load(self):
         try:
-            with open(self.filename, 'rb') as f:
-                self._accounts = pickle.load(f)
+            with open(self.filename, 'rb') as file:
+                self._accounts = pickle.load(file)
         except:
             self._accounts = []
 
-    def exist(self, roll):
-        if self._accounts:
-            for b in self._accounts:
-                if b.roll == roll:
-                    return True
-        return False
-
     def save(self):
-        if os.path.isfile(self.filename):
-            os.remove(self.filename)
-        with open(self.filename, 'wb') as f:
-            pickle.dump(self._accounts, f)
+        for i in os.listdir():
+            if self.filename == i:
+                os.remove(self.filename)
+        with open(self.filename, 'wb') as file:
+            pickle.dump(self._accounts, file)
 
-    def search(self, roll):
-        for pos, b in enumerate(self._accounts):
-            if b.roll == roll:
-                return pos, b
-        return 0, None
-
-
-    def add_account(self, roll):
-        name = input("Purpose: ")
-        password = input('Enter the password')
-        account = Student(roll, name, password)
+    def add_account(self, no, p=None, pas=None):
+        purp = p or input("Purpose: ")
+        password = pas or input('Enter the password')
+        key = Fernet.generate_key()
+        password = Fernet(key).encrypt(str.encode(password))
+        account = drop(no, purp, password, key)
         self._accounts.append(account)
         self.save()
 
-    def display(self, roll):
-        if self.exist(roll):
-            pos, account = self.search(roll)
-            print(account)
+    def display(self, no=None):
+        if no:
+            for i in self._accounts:
+                if no == i.no:
+                    print(
+                        f'Password: {Fernet(i.key).decrypt(i.password).decode()}')
         else:
-            print("account does not exist")
+            for i in self._accounts:
+                print(i)
 
-    def display_all(self):
+    def update(self, no):
         for i in self._accounts:
-            print(i)
-
-    def update(self, roll):
-        if self.exist(roll):
-            pos, account = self.search(roll)
-            print(account)
-            name = input('Name:\n')
-            self._accounts[pos].description = name
+            if input("Warning!\nOnce you press \'y\', you're old data will forever be gone.\nDo you want to continue?[y/n]").lower() == 'y':
+                if i.no == no:
+                    password = Fernet(i.key).decrypt(i.password).decode()
+                    self.delete(no)
+                    no = int(input("Enter new Roll no:"))
+                    purp = input("Enter new purpose:")
+                    self.add_account(no, purp, password)
+                    i.no = no
+                    i.purp = purp
             self.save()
-            print("successfully updated")
-        else:
-            print( "account does not exist")
 
-    def delete(self, roll):
-        if self.exist(roll):
-            pos, account = self.search(roll)
-            print(account)
-            confirm = input("Are you sure? (Y/N):\t").upper()
-            if confirm == 'Y':
-                del self._accounts[pos]
-                self.save()
-                print("successfully deleted")
-        else:
-            print("account does not exist")
+    def delete(self, no=None):
+        for pos, i in enumerate(self._accounts):
+            if no:
+                if i.no == no:
+                    confirm = input("Are you sure? (Y/N):\t").upper()
+                    if confirm == 'Y':
+                        del self._accounts[pos]
+                        self.save()
+                        print("successfully deleted")
+                else:
+                    print("account does not exist")
+            else:
+                for i in os.listdir():
+                    if i == self.filename:
+                        os.remove(self.filename)
 
 
 def menu():
     while True:
-        choice = int(input('1.Create\n2.Read\n3.Update\n4.Delete\n5.Display all\n0.Exit\n'))
-        accounts = Students()
-        if choice == 1:
-            roll = int(input('Enter the roll no.:\t'))
-            accounts.add_account(roll)
-        elif choice == 2:
-            roll = int(input('Enter roll:\t'))
-            accounts.display(roll)
-        elif choice == 3:
-            roll = int(input("Enter roll:\t"))
-            accounts.update(roll)
-        elif choice == 4:
-            roll = int(input("Enter roll:\t"))
-            accounts.delete(roll)
-        elif choice == 5:
-            accounts.display_all()
-        elif choice == 0:
+        choice = input(
+            '1.Create\n2.Read\n3.Update\n4.Delete\n5.Display all\n6.Delete All\n0.Exit\n')
+        accounts = main()
+        if choice == '1':
+            no = int(input('Enter Roll no.:\t'))
+            accounts.add_account(no)
+        elif choice == '2':
+            no = int(input('Enter Roll no:\t'))
+            accounts.display(no)
+        elif choice == '3':
+            no = int(input("Enter Roll no:\t"))
+            accounts.update(no)
+        elif choice == '4':
+            no = int(input("Enter Roll no:\t"))
+            accounts.delete(no)
+        elif choice == '5':
+            accounts.display()
+        elif choice == '6':
+            accounts.delete()
+        elif choice == '0':
             break
+
 
 if __name__ == '__main__':
     menu()
